7e2fbc9d0764781f2e519057ed8d440f1bcd1fd2
Florine <florinevinet@gmail.com>
2024-06-14 09:12:54 +0200

    projects datas added

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import "../styles/navbar.css"
+
+function NavBar (){
+    return (
+        <nav>
+            <ul>
+                <li>A propos</li>
+                <li>Projets</li>
+                <li>Expériences</li>
+                <li>Contact</li>
+            </ul>
+            {/* - contact : mail, linkedin, github */}
+        </nav>
+    )
+}
+
+export default NavBar;
